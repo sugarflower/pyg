25fbc5df047dab6fc2074d82105c880a640ff743
sugarflower <sse6611@gmail.com>
2019-07-04 00:04:08 +0900

    renew

---
+++ pyg/core.py
@@ -0,0 +1,222 @@
+from pygame import *
+from pygame.locals import *
+import pygame.gfxdraw as gfx
+import time, inspect, os
+
+FLIP_H = 1
+FLIP_V = 2
+FLIP_BOTH = 3
+
+_running = True
+_keys = [False] * 512
+_keyDownCnt = 0
+_mouse = {"pos":[0,0], "button":[False]*3, "click":[False]*3 }
+_size = {"real":[128,128],"disp":[384,384],"margin":[20,20]}
+_screen = {"offset":[0,0]}
+_waitTime = 0.016
+_mode = HWSURFACE #| DOUBLEBUF
+_fullscreen = False
+screen = None
+surface = None
+Event = None
+
+def setSize( real=None, disp=None, margin=None ):
+	if real != None:
+		_size["real"] = real
+	if disp != None:
+		_size["disp"] = disp
+	if margin != None:
+		_size["margin"] = margin
+
+def dispMouseCursor(val):
+	mouse.set_visible(val)
+
+def setTitle(val):
+	display.set_caption(val)
+
+def setWaitTime(val=0.016):
+	global _waitTime
+	_waitTime = val
+
+def setOffset(offset):
+	global _screen
+	_screen["offset"] = offset
+
+def isKeyDown(key):
+	return _keys[ord(key)]
+
+def isKeySownCode(key):
+	return _keys[key]
+
+def isAnyKeyDown():
+	return _keyDownCnt != 0
+
+def getMousePos():
+	wa = _size["real"][0] / _size["disp"][0]
+	ha = _size["real"][1] / _size["disp"][1]
+	mx = _mouse["pos"][0] - _size["margin"][0]
+	my = _mouse["pos"][1] - _size["margin"][1]
+	return ( int(mx * wa), int(my * ha) )
+
+def isClick(button=0):
+	return _mouse["click"][button]
+
+def isPress(button=0):
+	return _mouse["button"][button]
+
+def clear():
+	fill((0,0,0,0),surface)
+	fill((0,0,0),screen)
+
+def fill( color=(0,0,0,0), surf=None ):
+	if surf == None:
+		surf = surface
+	surf.fill(color)
+
+def loadImage(path):
+	return image.load(path).convert_alpha()
+
+def createImage(size):
+	return Surface(size, _mode | SRCALPHA, 32)
+
+def getEvent():
+	return Event
+
+def putImage(img, pos, rect=None, flip=None, rotate=None, surf=None, scale=None, center=False):
+	if surf == None:
+		surf = surface
+
+	if rect == None:
+		imgrect = ( img.get_width(), img.get_height())
+	else:
+		imgrect = (rect[2], rect[3])
+	surfTemp = Surface( imgrect, _mode | SRCALPHA, 32 )
+	surfTemp.blit(img,(0,0))
+
+	if flip != None:
+		if flip == FLIP_H:
+			surfTemp = transform.flip(surfTemp, True, False)
+		elif filp == FLIP_V:
+			surfTemp = transform.flip(surfTemp, False,True)
+		elif flip == FLIP_BOTH:
+			surfTemp = transform.flip(surfTemp, True,True)
+	
+	if rotate != None:
+		rotate = (360 - rotate) % 360
+		st2 = transform.rotate(surfTemp, rotate)
+		w = surfTemp.get_width()
+		h = surfTemp.get_height()
+		dw = int((w - st2.get_width()))>>1
+		dh = int((h - st2.get_height()))>>1
+	else:
+		st2 = surfTemp
+		dw = 0
+		dh = 0 
+	
+	if scale != None:
+		sc = (int(st2.get_width()*scale[0]), int(st2.get_height()*scale[1]))
+		st3 = transform.scale(st2, sc)
+	else:
+		st3 = st2
+
+	if center:
+		surf.blit(st3,(pos[0]+dw-(w>>1), pos[1]+dh-(h>>1)))
+	else:
+		surf.blit(st3,(pos[0]+dw, pos[1]+dh))
+		
+def _init():
+	mixer.pre_init( 44100, -16, 2, 1024 )
+	mixer.init()
+	init()
+	os.environ["SDL_VIDEO_CENTERED"] = "1"
+
+def _setupDisplay(fullscreen=False):
+	global screen, surface
+	dispW = _size["disp"][0] + (_size["margin"][0] * 2)
+	dispH = _size["disp"][1] + (_size["margin"][1] * 2)
+	screen  = display.set_mode( (dispW, dispH), _mode)
+	surface = Surface(_size["real"], _mode | SRCALPHA , 32 )
+	if fullscreen:
+		display.toggle_fullscreen()
+	dispMouseCursor(False)
+
+def _update():
+	us = Surface(_size["real"], _mode | SRCALPHA, 32)
+	us.blit(surface, _screen["offset"] )
+	screen.blit(transform.scale(us, _size["disp"]) , _size["margin"])
+	display.update()
+
+def _process():
+	global _running, _mouse, _keys, _keyDownCnt, Event, _fullscreen
+	st = time.time()
+	_procWait = True
+	_mouse["click"] = [False] * 3
+	Event = None
+	while _procWait:
+		time.sleep(0.001)
+		for e in event.get():
+			Event = e
+			_key = -1
+
+			if e.type == QUIT:
+				_running = False
+
+			if e.type == KEYDOWN:
+				_key = e.key
+				if _key < 512:
+					if _keys[_key] == False:
+						_keyDownCnt += 1
+						_keys[_key] = True
+
+			if e.type == KEYUP:
+				_key = e.key
+				if _key < 512:
+					if _keys[_key]:
+						_keyDownCnt -= 1
+						_keys[_key] = False
+				if _key == 292:
+					if _fullscreen:
+						_fullscreen = False
+						_setupDisplay()
+					else:
+						_fullscreen = True
+						_setupDisplay(True)
+				if _key == 27:
+					_running = False
+
+			if e.type == MOUSEMOTION:
+				_mouse["pos"] = (e.pos[0], e.pos[1])
+
+			if e.type == MOUSEBUTTONDOWN:
+				_mouse["button"] = mouse.get_pressed()
+
+			if e.type == MOUSEBUTTONUP:
+				for i in range(3):
+					if _mouse["button"][i]:
+						_mouse["click"][i] = True
+					_mouse["button"] = [False] * 3
+
+		if time.time() - st > _waitTime:
+			_update()
+			_procWait = False
+
+def run():
+	s = inspect.stack()
+	p = inspect.getmodule( s[ len(s)-1 ][0] )
+	d = set(dir(p))
+
+	if len(d & {"setup"} ) == 1:
+		p.setup()
+	
+	_setupDisplay()
+
+	if len(d & {"loop"} ) == 1:
+		while _running:
+			_process()	
+			p.loop()
+	
+	quit()
+
+_init()
+_setupDisplay()
+
--- test.py
+++ test.py
@@ -1,111 +1,27 @@
-#!/usr/bin/python3
-import pyg 
+#!/usr/bin/python
+from pyg import *
+import pyg.font8x8 as font
+import random
 
-"""
-pyg.init
-set real picture size and display size
-"""
-pyg.begin( (80,80), (320,320) )
-pyg.setTitle("*pyg test*")
+setSize((160,160),(320,320))
+setTitle("PYG2 Test")	
+img = loadImage("res/pic.png")
 
-"""
-get resource and initialize for application
-"""
-img = pyg.imgLoad("res/pic.png")
-pos = [0, 0] 
-r = 0
+def loop():
+	clear()
+	fill(Color(100,0,0))
+	font.setPos(20,80)
+	font.printStr("hello world")
+	putImage(img,(50,50))
+	putImage(img,(0,0))
+	setOffset((random.randint(0,10)-5,random.randint(0,10)-5))
 
-"""
-define for sprite sheet
-"name" : ( pos x, pos y , width , height )
-"""
-sp = {
-	"player":(0, 0, 8, 8), 
-	"tomato":(8, 0, 8, 8),
-	"block":(16,0,8,8),
-}
-
-"""
-Level design Sample
-create "bg" and drow it.
-"""
-map = (
-	0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,
-	1,0,0,0,0,0,0,0,0,0,
-	1,0,0,0,0,0,0,0,0,1,
-	1,0,0,0,0,1,0,0,0,1,
-	1,0,0,0,0,0,0,1,0,1,
-	1,1,1,1,1,1,1,1,1,1,
-)
-
-bg = pyg.createImg((80,80))
-for y in range(10):
-	for x in range(10):
-		idx = y*10+x
-		if map[idx] == 1:
-			pyg.putImg(img, (x*8, y*8), sp["block"],surf=bg)
-
-
-"""
-font
-"""
-f = pyg.TinyFont()
-
-"""
-main loop
-"""
-while pyg._running:
-	pyg.process(0.016)
-	pyg.fill((0,0,0,32))
-
-	pyg.putImg(bg, (40,40), rotate=r, center=True ) #draw bg
-	r = (r + 2) % 360
-
-	pyg.putImg(img, (0,0))
-
-	pyg.putImg(img, (0,16), sp["tomato"])
-	pyg.putImg(img, (8,16), sp["tomato"], flip = pyg.FLIP_H)
-	pyg.putImg(img, (16,16),sp["tomato"], flip = pyg.FLIP_V)
-	pyg.putImg(img, (24,16),sp["tomato"], flip = pyg.FLIP_BOTH)
-
-	pyg.putImg(img, (0,24), sp["player"], rotate = 90 )
-	pyg.putImg(img, (8,24), sp["player"], rotate = 180)
-	pyg.putImg(img, (16,24),sp["player"], rotate = 270)
-
-	pyg.putImg(img,pyg.get_mousePos(), sp["player"])
-
-	f.print("PYG TEST",(4,36))
-	f.print("by SUGARFLOWER",(20,42))
-
-
-	if pyg.isKeyDownCode(27): #ESC key
-		pyg._running = False
-	
-	if pyg.isKeyDown("a"):
-		print("true")
-	
-	if pyg.isAnyKeyDown():
-		print("any")
-
-	if pyg.isClick():
-		print("click")
-	if pyg.isClick(1):
-		print("click 1")
-	if pyg.isClick(2):
-		print("click 2")
-
-	if pyg.isPress():
-		print("press")
-	if pyg.isPress(1):
-		print("press 1")
-	if pyg.isPress(2):
-		print("press 2")
-"""
-end app
-"""
-pyg.quit()
+	"""
+	e = getEvent()
+	if e != None:
+		print(e)
+		if e.type == 6:
+			print(e.button)
+	"""
+run()
 
